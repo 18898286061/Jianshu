8b3890c3b6a718f10b827a7bc52adcdb39c31b97
KaiKai <1035759181@qq.com>
2019-04-27 13:45:25 +0800

    init Project & 重写代码 & 引入StyleComponet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './common/header';
-import Home from './pages/home';
-import Detail from './pages/detail/loadable.js';
-import Login from './pages/login';
-import Write from './pages/write';
-import store from './store/';
+import React from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <BrowserRouter>
-                    <div>
-                        <Header/>
-                        <Route path='/' exact component={Home}></Route>
-                        <Route path='/login' exact component={Login}></Route>
-                        <Route path='/write' exact component={Write}></Route>
-                        <Route path='/detail/:id' exact  component={Detail}></Route>
-                    </div>
-                </BrowserRouter>
-            </Provider>
-        );
-    }
+class App extends React.Component{
+  render(){
+    return <h1>Hello React</h1>
+  }
 }
+
 export default App;
